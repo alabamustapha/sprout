53a0077ae7148db7cc83ab0dbffa048567c8077b
Alaba Mustapha <alabamustapha@gmail.com>
2018-11-27 18:13:26 +0000

    Nova installed

--- config/auth.php
+++ config/auth.php
@@ -65,15 +65,11 @@ return [
     */
 
     'providers' => [
-        // 'users' => [
-        //     'driver' => 'eloquent',
-        //     'model' => App\User::class,
-        // ],
-
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\User::class,
         ],
+
 
         // 'users' => [
         //     'driver' => 'database',
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,64 +2,145 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="row">
+       <div class="col-md-12 mb-3">
+            <form>
+                <div class="form-row">
+                    <div class="col-7">
+                        <input type="text" class="form-control" placeholder="Name">
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="State" value="Kaduna" disabled>
+                    </div>
+                    <div class="col">
+                        <select name="lga_name" id="lga_name" class="form-control">
+                            <option value="">All</option>
+                            <option value="Birnin Gwari">Birnin Gwari</option>
+                            <option value="Chikun">Chikun</option>
+                            <option value="Giwa">Giwa</option>
+                            <option value="Igabi">Igabi</option>
+                            <option value ="Ikara">Ikara</option>
+                            <option value ="Jaba">Jaba</option>
+                            <option value ="Jema’A">Jema’a</option>
+                            <option value ="Kachia">Kachia</option>
+                            <option value ="Kaduna North">Kaduna North</option>
+                            <option value ="Kaduna South">Kaduna South</option>
+                            <option value ="Kagarko">Kagarko</option>
+                            <option value ="Kajuru">Kajuru</option>
+                            <option value ="Kaura">Kaura</option>
+                            <option value ="Kauru">Kauru</option>
+                            <option value ="Kubau">Kubau</option>
+                            <option value ="Kudan">Kudan</option>
+                            <option value ="Lere">Lere</option>
+                            <option value ="Makarfi">Makarfi</option>
+                            <option value ="Sabon Gari">Sabon Gari</option>
+                            <option value ="Sanga">Sanga</option>
+                            <option value ="Soba">Soba</option>
+                            <option value ="Zangon Kataf">Zangon Kataf</option>
+                            <option value ="Zaria">Zaria</option>
+                        </select>
+                    </div>
                 </div>
-            </div>
+            </form>
+       </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-12" id="facilities">
+            
         </div>
     </div>
+    
+    <div class="row justify-content-center">
+        <button class="btn btn-lg btn-primary" id="load-more" data-url="">Load more</button>
+    </div>
+
+    
 </div>
 @endsection
 
 
 
 @section('footer_scripts')
-
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
 <script>
 
 
     let url = "https://api.grid-nigeria.org/"
+    url = "https://api.grid-nigeria.org/health-facilities/?cql=state_name IN ('Kaduna') and lga_name = 'Lere'&size=4"
 
-    const getDatasets = (resource_name, size = 200, page = 1, sort_by = "global_id", fields = "", cql = "") => {
-        const endpoint = `${url}${resource_name}`
-        axios.get(endpoint, {
-            params: {
-                size: size,
-                page: page,
-                sort_by: sort_by,
-                fields: fields,
-                cql: cql,
-                crossdomain: true
-            }
-        }).then(response => {
-            console.log(response.data)
-            return response.data
+    const getDatasets = (url) => {
+        
+        axios.get(url).then(response => {
+            
+            console.log(response)
+            features = response.data.features
+            
+                let resource = `
+                <div class="card-deck mb-5">
+        
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${features[0].properties.name}</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${features[1].properties.name}</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${features[2].properties.name}</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${features[3].properties.name}</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+
+                </div>                      
+                `
+
+                $('#facilities').append(resource)
+
+                $('#load-more').attr('data-url', response.data.pager.next);
+                
         })
     }
-
-    let resource_name = "schools" // Name of endpoint
-    let size = 200 // Number of items to return
-    let page = 1 // Page Number to return
-    let sort_by = "lga_name" // Field used in sorting responses
-    let fields = "lga_name,state_name,ward_name" // Comma-separated list of fields (No space)
-    let cql = "state_name IN ('Kaduna', 'Kano')" // Read documentations about Querying/Filtering (CQL)
-
-    let response = getDatasets(resource_name=resource_name, size=size, cql=cql) // You can optionally pass other parameters as defined above
+ 
+    let response = getDatasets(url) // You can optionally pass other parameters as defined above
 
     console.log(response)
+
+
+    $(document).ready(function(){
+        $("#load-more").click(function(event){
+            event.preventDefault()
+            alert("clicked")
+            let response = getDatasets($(this).data('url')) // You can optionally pass other parameters as defined above
+        })    
+        
+        $("[name='lga_name']").change(function(event){
+            let lga_name = $(this).val()
+            url = "https://api.grid-nigeria.org/health-facilities/?size=4&cql=state_name IN ('Kaduna') and lga_name = '" + lga_name + "'"
+            $('#facilities').empty()
+            
+            getDatasets(url) // You can optionally pass other parameters as defined above
+            
+        })    
+    })
+    
 
 </script>
 
