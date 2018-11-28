100ad9cde88cc20b4d44a2c581a62fe821ccdcc0
Dalhatu <kulgee001@yahoo.com>
2018-11-28 21:35:15 +0100

    fixes seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(Designations::class);
         $this->call(Drugs::class);
         $this->call(Services::class);
+        $this->call(Users::class);
     }
 }
--- database/seeds/Designations.php
+++ database/seeds/Designations.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Designation;
 
 class Designations extends Seeder
 {
